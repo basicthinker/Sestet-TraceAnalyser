b159984553fcdc2cb77caa59651cdc1757e454e5
Jinglei Ren <jinglei.ren@gmail.com>
2013-11-26 11:14:53 -0500

    Add header for kernel struct.

---
+++ kernel_struct.h
@@ -0,0 +1,38 @@
+// kernel_struct.h
+// Sestet-TraceAnalyser
+//
+// Jinglei Ren <jinglei.ren@gmail.com>
+// Nov. 26, 2013
+
+#ifndef SESTET_TRACE_ANALYSER_KERNEL_STRUCT_H_
+#define SESTET_TRACE_ANALYSER_KERNEL_STRUCT_H_
+
+struct timeval32 {
+  uint32_t tv_sec;
+  uint32_t tv_usec;
+};
+
+inline double tv_float(const struct timeval32 &tv) {
+  return tv.tv_sec + (double)tv.tv_usec / 1000000;
+}
+
+struct te_page {
+  char te_type;
+  char te_hit;
+  uint32_t te_ino;
+  uint32_t te_pgi;
+};
+
+#define PAGE_SIZE 4096
+
+#define TE_OP_READ  'r'
+#define TE_OP_WRITE 'w'
+#define TE_OP_FSYNC 's'
+#define TE_OP_EVICT 'e'
+
+#define TE_HIT_YES     'y'
+#define TE_HIT_NO      'n'
+#define TE_HIT_UNKNOWN 'u'
+
+#endif // SESTET_TRACE_ANALYSER_KERNEL_STRUCT_H_
+
--- simu_core.h
+++ simu_core.h
@@ -1,4 +1,6 @@
+// simu_core.h
 // Sestet-TraceAnayliser
+//
 // Jinglei Ren <jinglei.ren@stanzax.org>
 // Nov. 25, 2013
 
@@ -8,34 +10,17 @@
 #include <stdint.h>
 #include <set>
 
+#include "kernel_struct.h"
+
 typedef std::pair<unsigned long, double> PageTag;
-
-#define PAGE_SIZE 4096
-
-struct te_page { // kernel struct Project Sestet-AdaFS
-  char te_type;
-  char te_hit;
-  uint32_t te_ino;
-  uint32_t te_pgi;
-};
 
 class SimuCore {
   public:
-    SimuCore();
     void Input(char operation, const struct te_page &page);
     virtual void OnRead(const struct te_page &page, bool hit) = 0;
     virtual void OnWrite(const struct te_page &page, bool hit) = 0;
+    virtual ~SimuCore();
 
-    // Counterparts of kernel definitions
-    const char kOpRead = 'r';
-    const char kOpWite = 'w';
-    const char kOpFsync = 's';
-    const char kOpEvict = 'e';
-
-    const char kHitYes = 'y';
-    const char kHitNo = 'n';
-    const char kHitUnkown = 'u';
- 
   private:
     std::set<PageTag> cache_; // simulates page cache
 };
