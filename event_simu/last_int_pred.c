3ecb413fe5d7836cf547643fcf63fb219a75f18f
Jinglei Ren <jinglei.ren@gmail.com>
2013-12-04 12:32:08 -0500

    Fix bug in last model and rename its file.

--- event_simu/last_int_pred.c
+++ event_simu/last_int_pred.c
@@ -19,39 +19,44 @@ int num_pred[NUM_TESTS];
 double last_pred[NUM_TESTS];
 double total_len[NUM_TESTS];
 
-static void init_pred(double pred) {
+static void init_pred() {
   int i;
   for (i = 0; i < NUM_TESTS; ++i) {
     num_confl[i] = 0;
     num_pred[i] = 0;
-    last_pred[i] = pred;
-    total_len[i] = 0.0;
+    last_pred[i] = 0;
+    total_len[i] = 0;
   }
 }
 
-static void check_conflict(double in) {
+static void check_conflict(double past_int) {
   int i;
   for (i = 0; i < NUM_TESTS; ++i) {
-    if (in < last_pred[i]) {
+    last_pred[i] -= past_int;
+    if (last_pred[i] > 0) {
+      fprintf(stderr, "\t[%d] flush remains %f\n", i, last_pred[i]);
       ++num_confl[i];
+    } else {
+      fprintf(stderr, "\t[%d] flush ends.\n", i);
     }
-    last_pred[i] -= in;
   }
 }
 
-static void try_pred(struct adafs_interval_history *int_hist, double in) {
+static void try_pred(struct adafs_interval_history *int_hist) {
   if (last_pred[TEST_MIN] <= 0) {
     double min = 1024, *pos;
-    for_each_history(pos, int_hist) {
+    for_each_history_limit(pos, int_hist, fh_len(int_hist)) {
       if (*pos < min) min = *pos;
     }
     last_pred[TEST_MIN] = min;
+    fprintf(stderr, "\t[%d] flush begins %f\n", TEST_MIN, min);
     ++num_pred[TEST_MIN];
     total_len[TEST_MIN] += min;
   }
 
   if (last_pred[TEST_AVG] <= 0) {
     last_pred[TEST_AVG] = fh_state(int_hist) / fh_len(int_hist);
+    fprintf(stderr, "\t[%d] flush begins %f\n", TEST_AVG, last_pred[TEST_AVG]);
     ++num_pred[TEST_AVG];
     total_len[TEST_AVG] += last_pred[TEST_AVG];
   }
@@ -67,8 +72,9 @@ static void print_stat(char *filename) {
 }
 
 int main(int argc, char *argv[]) {
+  int i;
   struct adafs_interval_history int_hist = ADAFS_HISTORY_INIT(LEN_BITS, 0.0, 0.0);
-  double next_int;
+  double past_int;
   if (argc != 2) {
     fprintf(stderr, "Usage: %s EventLog\n",
         argv[0]);
@@ -77,12 +83,15 @@ int main(int argc, char *argv[]) {
   
   freopen(argv[1], "r", stdin);
   
-  init_pred(THR_INT);
-  while (scanf("%lf", &next_int) == 1) {
-    fh_update_interval(&int_hist, next_int);
-    check_conflict(next_int);
-    try_pred(&int_hist, next_int);
-  }
+  init_pred();
+  fh_update_interval(&int_hist, THR_INT);
+  try_pred(&int_hist);
+  while (scanf("%lf", &past_int) == 1) {
+    fprintf(stderr, "%f\n", past_int);
+    check_conflict(past_int); // review
+    fh_update_interval(&int_hist, past_int);
+    try_pred(&int_hist);
+ }
   print_stat(argv[1]);
   return 0;
 }
