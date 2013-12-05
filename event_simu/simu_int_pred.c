e7b962bb040614132cd6264e60f79836a5fd6681
Jinglei Ren <jinglei.ren@gmail.com>
2013-12-04 20:12:15 -0500

    Verify simu_int_pred.c.

--- event_simu/simu_int_pred.c
+++ event_simu/simu_int_pred.c
@@ -38,93 +38,98 @@ char *event_name[] = {
 #define min_time(a, b) ((a) < (b) ? (a) : (b))
 
 static inline void print_event(enum event ev, double past, enum state s) {
-  fprintf(stderr, "%s on %s=%f => ", state_name[s], event_name[ev], past);
+  fprintf(stderr, "%s on %s=%f\n", state_name[s], event_name[ev], past);
 }
 
 static inline void print_state(enum state ns, double timer) {
-  fprintf(stderr, "%s timer=%f\n", state_name[ns], timer);
+  fprintf(stderr, "=> %s timer=%f\n", state_name[ns], timer);
 }
 
-#define update_hist_int(ts, next_int) \
-    (fh_update_interval(&(ts)->int_hist, (next_int)))
-
-#define set_timer(ts) ({ \
+#define hist_avg(ts) ({ \
     struct adafs_interval_history *fh = &(ts)->int_hist; \
-    (ts)->timer = fh_state(fh) / fh_len(fh); })
+    fh_state(fh) / fh_len(fh); })
 
-static inline double threshold(struct adafs_touch_state ts_vec[]) {
-  double timer = set_timer(&ts_vec[ST_SHORT_INT]);
-  return timer * THRE_MULTI;
+#define threshold(ts_vec) (hist_avg((ts_vec) + ST_SHORT_INT) * THRE_MULTI)
+
+static inline void update_hist(struct adafs_touch_state *ts, double past_int) { 
+    fh_update_interval(&ts->int_hist, past_int);
+    ts->timer = hist_avg(ts);
 }
 
 int num_conflicts = 0;
 int num_pred=0;
+double last_pred = 0.0;
 double total_len = 0.0;
 
-static inline double pred_int(struct adafs_touch_state ts_vec[]) {
-  double pred = set_timer(&ts_vec[ST_LONG_INT]);
-  ++num_pred;
-  total_len += pred;
+static inline double try_pred(struct adafs_touch_state ts_vec[]) {
+  double pred = ts_vec[ST_LONG_INT].timer;
+  if (last_pred <= 0) {
+    ++num_pred;
+    total_len += pred;
+    last_pred = pred;
+    fprintf(stderr, "\tFlush begins %f\n", pred);
+  } else {
+    fprintf(stderr, "\tFlush remains %f\n", last_pred);
+  }
   return pred;
 }
 
-static inline void check_conflict(struct adafs_touch_state ts_vec[],
-      const double next_int) {
-  if (next_int < ts_vec[ST_LONG_INT].timer) {
+static inline void check_conflict() {
+  if (last_pred > 0) {
     ++num_conflicts;
-    ts_vec[ST_LONG_INT].timer -= next_int;
-  } else {
-    ts_vec[ST_LONG_INT].timer = 0;
+    fprintf(stderr, "\tFlush remains %f\n", last_pred);
   }
 }
 
+// When event is from timer, @past_int is the last timer value;
+// when event is from user, @past_int is the last remaining interval excluding timer values.
 // Returns timer value
 double transfer(struct adafs_touch_state ts_vec[],
-    enum state *s, enum event ev, double next_int) {
+    enum state *s, enum event ev, double past_int) {
   switch (*s) {
   case ST_SHORT_INT:
     if (ev == EV_USER) {
-      check_conflict(ts_vec, next_int);
-      update_hist_int(&ts_vec[ST_SHORT_INT], next_int);
+      check_conflict();
+      update_hist(&ts_vec[ST_SHORT_INT], past_int);
       return threshold(ts_vec);
     } else if (ev == EV_TIMER) {
       *s = ST_LONG_INT;
-      return pred_int(ts_vec);
-    } else fprintf(stderr, "Invalid event %d on state %d\n", ev, *s);
+      return try_pred(ts_vec);
+    }
     break;
   case ST_LONG_INT:
     if (ev == EV_USER) {
-      check_conflict(ts_vec, next_int);
-      if (next_int <= threshold(ts_vec)) {
+      check_conflict();
+      if (past_int <= threshold(ts_vec)) {
         *s = ST_SHORT_INT;
-        update_hist_int(&ts_vec[ST_SHORT_INT], next_int);
+        update_hist(&ts_vec[ST_SHORT_INT], past_int);
         return threshold(ts_vec);
       } else {
         *s = ST_SHORTER;
-        update_hist_int(&ts_vec[ST_LONG_INT], next_int);
+        update_hist(&ts_vec[ST_LONG_INT], past_int);
         return threshold(ts_vec);
       }
     } else if (ev == EV_TIMER) {
       *s = ST_LONGER;
       return TIME_WAIT;
-    } else fprintf(stderr, "Invalid event %d on state %d\n", ev, *s); 
+    }
     break;
   case ST_SHORTER:
     if (ev == EV_USER) {
-      check_conflict(ts_vec, next_int);
+      check_conflict();
       *s = ST_SHORT_INT;
-      update_hist_int(&ts_vec[ST_SHORT_INT], next_int);
+      update_hist(&ts_vec[ST_SHORT_INT], past_int);
       return threshold(ts_vec);
     } else if (ev == EV_TIMER) {
       *s = ST_LONG_INT;
-      return pred_int(ts_vec);
-    } else fprintf(stderr, "Invalid event %d on state %d\n", ev, *s);
+      return try_pred(ts_vec);
+    }
     break;
   case ST_LONGER:
     if (ev == EV_USER) {
-      check_conflict(ts_vec, next_int);
+      check_conflict();
       *s = ST_SHORT_INT;
-      update_hist_int(&ts_vec[ST_LONG_INT], next_int);
+      update_hist(&ts_vec[ST_LONG_INT], past_int);
       return threshold(ts_vec);
     } else fprintf(stderr, "Invalid event %d on state %d\n", ev, *s);
     break;
@@ -135,7 +140,7 @@ double transfer(struct adafs_touch_state ts_vec[],
 int main(int argc, char *argv[]) {
   struct adafs_touch_state ts_vec[2] = { ADAFS_TOUCH_STATE_INIT(LEN_BITS), ADAFS_TOUCH_STATE_INIT(LEN_BITS) };
   enum state s;
-  double next_int;
+  double past_int;
   double timer;
 
   if (argc != 3) {
@@ -147,22 +152,25 @@ int main(int argc, char *argv[]) {
   freopen(argv[1], "r", stdin);
   THRE_MULTI = atof(argv[2]);
 
-  update_hist_int(&ts_vec[ST_SHORT_INT], INIT_SHORT);
-  update_hist_int(&ts_vec[ST_LONG_INT], INIT_LONG);
+  update_hist(&ts_vec[ST_SHORT_INT], INIT_SHORT);
+  update_hist(&ts_vec[ST_LONG_INT], INIT_LONG);
 
   s = ST_SHORT_INT;
   timer = threshold(ts_vec);
-  while (scanf("%lf", &next_int) == 1) {
-    fprintf(stderr, "%f\n", next_int);
-    while (next_int > timer) {
-      next_int -= timer;
+  while (scanf("%lf", &past_int) == 1) {
+    double rest_int = past_int;
+    fprintf(stderr, "%f\n", past_int);
+    while (rest_int > timer) {
       print_event(EV_TIMER, timer, s);
+      rest_int -= timer;
+      last_pred -= timer;
       timer = transfer(ts_vec, &s, EV_TIMER, timer);
       if (timer < 0) return -EINVAL;
       print_state(s, timer);
     }
-    print_event(EV_USER, next_int, s);
-    timer = transfer(ts_vec, &s, EV_USER, next_int);
+    print_event(EV_USER, past_int, s);
+    last_pred -= rest_int;
+    timer = transfer(ts_vec, &s, EV_USER, rest_int);
     if (timer < 0) return -EINVAL;
     print_state(s, timer);
   }
@@ -171,5 +179,4 @@ int main(int argc, char *argv[]) {
       total_len, total_len/num_pred);
   return 0;
 }
-
 
